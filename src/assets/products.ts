baa4eaba1bfd1a7316aee4366322711356683013
mamonten0k <asdnbvb123@gmail.com>
2022-07-18 21:56:52 +0300

    refractor: clear assets, move sample products data to assets

---
+++ src/assets/products.ts
@@ -0,0 +1,162 @@
+import { albumProps } from "../types/interfaces"
+
+export const sampleData:albumProps[] = [{
+	id: "1",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "2",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "3",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "4",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "5",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "6",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "7",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "8",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "9",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+},
+{
+	id: "10",
+	cover: "https://i.scdn.co/image/ab67616d0000b27346e207de66ba06422897f769",
+	author: "Linkin Park",
+	description: `Minutes to&nbsp;Midnight is&nbsp;the band&rsquo;s follow-up album
+	to&nbsp;Meteora (2003), and features a&nbsp;shift in&nbsp;the
+	group&rsquo;s musical direction. For the band, the album marked
+	a&nbsp;beginning of&nbsp;deviation from their signature nu&nbsp;metal
+	sound. Minutes to&nbsp;Midnight takes its title from the Doomsday
+	Clock symbol. It&nbsp;is&nbsp;also the band&rsquo;s first full-length
+	album to&nbsp;carry a&nbsp;Parental Advisory label.`,
+	jenres: ["metal"],
+	releaseDate: "2007",
+	trackList: ["Wake", "Given Up", "Live Out All The Rest", "Bleed It Out", "Shadow of the Day", "What I've Done", "Hands Held High", "No More Sorrow"],
+	title: "Minutes To Midnight",
+}]
